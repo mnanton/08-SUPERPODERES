42fc840d66189e9cc150705e3332655827c77c99
Miguel <michelnd@hotmail.com>
2023-11-12 18:55:13 +0000

    Initial Commit

---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by Miguel Nantón Díaz on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
